a3486f9be29e0aa96f7b74790689525645338ac0
dabarrett42@students.tntech.edu <dabarrett42@students.tntech.edu>
2014-09-07 14:21:01 -0500

    Keyboard.h

--- ReadFile.h
+++ ReadFile.h
@@ -14,10 +14,19 @@ private:
    bool closed;
 
 public:
+//!constructor, which reads in the file
 ReadFile(const char* file_name);
+
+//!destructor, which closes the file
 ~ReadFile();
+
+//!function necessary for obtaining lines of the file
 String* readLine();
+//!tracks the file to see if the end of the file has been reached
 bool eof();
+
+/*!confirms that the file is actually closed, and closes 
+the file if it isn't closed already*/
 void close();
 };
 #endif
